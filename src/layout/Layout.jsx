6add75e75f9584e877ca65a0d6cccd2cc9a7ef40
Matias Ramirez <matiasgr3009@gmail.com>
2025-11-17 07:35:49 -0300

    formatting

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,20 +1,16 @@
-import {Outlet} from "react-router";
-import {Footer, Navbar} from "../components";
+import { Outlet } from "react-router";
+import { Footer, Navbar } from "../components";
 
 const Layout = () => {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-900 text-white">
-
-      <Navbar />
-
-      <main className="flex-grow pt-12 pb-12 px-4">
-        <Outlet />
-      </main>
-
-      <Footer />
-
-    </div>
-  );
+    return (
+        <div className="min-h-screen flex flex-col bg-gray-900 text-white">
+            <Navbar />
+            <main className="flex-grow pt-12 pb-12 px-4">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import RouterProvider from './Router.jsx'
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import RouterProvider from "./Router.jsx";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <RouterProvider />
-  </Provider>
-)
+createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <RouterProvider />
+    </Provider>
+);
