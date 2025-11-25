df0cbbea97f4fbd08bbe5b8aa0b87bdff9704c12
Gabriel Boubonis <boubonisgabriel@gmail.com>
2025-11-25 07:53:18 -0300

    login execution

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,7 +5,9 @@ import NavbarResoluciones from "../components/forms/NavbarResolucionesForm";
 const Layout = () => {
     return (
         <div className="min-h-screen flex flex-col bg-gray-900 text-white">
-            
+            <Modal>
+                <LoginForm/>
+            </Modal>
             <NavbarResoluciones />
             <main className="flex-grow pt-12 pb-12 px-4">
                 <Outlet />
