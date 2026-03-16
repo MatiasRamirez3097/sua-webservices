4d5e2fbb622097fc7b8cd9d6857c0cb20764e96c
Gabriel Boubonis <boubonisgabriel@gmail.com>
2026-03-16 07:19:33 -0300

    reworking of Textarea

--- src/components/textarea/TextArea.jsx
+++ src/components/textarea/TextArea.jsx
@@ -1,10 +1,10 @@
-const TextArea = ({ 
-    label, 
-    name, 
-    onChange, 
-    placeholder, 
+const TextArea = ({
+    label,
+    name,
+    onChange,
+    placeholder,
     value,
-    className="w-full p-3 rounded-lg bg-gray-700 text-white border border-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 resize-y", 
+    className = "w-full p-3 rounded-lg bg-gray-700 text-white border border-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 resize-y",
 }) => {
     return (
         <>
