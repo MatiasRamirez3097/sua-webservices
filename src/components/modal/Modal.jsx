618bcc32428f32cd2098cdb5abb3320f444bad7a
Gabriel Boubonis <boubonisgabriel@gmail.com>
2026-04-01 10:52:27 -0300

    modal modification

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -6,18 +6,12 @@ const Modal = ({ children }) => {
             role="dialog"
             aria-modal="true"
         >
-            {/* Fondo oscurecido */}
             <div className="fixed inset-0 bg-gray-800 bg-opacity-60 transition-opacity"></div>
 
-            {/* Contenedor principal */}
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                 <div className="flex min-h-full items-center justify-center p-4 text-center">
-
-                    {/* Caja del modal */}
-                    <div className="relative transform overflow-hidden rounded-xl bg-gray-900 text-white shadow-xl transition-all w-full max-w-lg p-8">
-
+                    <div className="relative transform overflow-hidden rounded-xl bg-gray-900 text-white shadow-xl transition-all w-full max-w-4xl p-8">
                         {children}
-
                     </div>
                 </div>
             </div>
