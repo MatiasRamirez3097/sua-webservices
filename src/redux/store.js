cc782080665e82c07cd05fa22031e69d2a62396c
Gabriel Boubonis <boubonisgabriel@gmail.com>
2026-04-11 17:44:15 -0300

    clean store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/auth/authSlice.js";
 import batchReducer from "./slices/sua/batchSlice.js";
 import userReducer from "./slices/auth/userSlice.js";
-/*export const store = configureStore({
-    reducer: {
-        batchsReducer,
-        usersReducer,
-    },
-});*/
 
 export const store = configureStore({
     reducer: {
